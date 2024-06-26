97ff11756d4a49d8d3be39f41a609a4c2fd07b92
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-06-26 13:43:48 +1200

    Create a simple Interface and Repository implementing the Interface

---
+++ Interface/IMenuRepository.cs
@@ -0,0 +1,13 @@
+﻿using MenuRestaurant.Models;
+
+namespace MenuRestaurant.Interface
+{
+    public interface IMenuRepository
+    {
+        List<Menu> GetAllMenus();
+        Menu GetMenuById(Guid Id);
+        Menu DeleteMenuById(Guid Id);
+        Menu PostMenu(Menu menu);
+        Menu UpdateMenuById(Guid Id, Menu menu);
+    }
+}
---
+++ Repository/MenuRepository.cs
@@ -0,0 +1,51 @@
+﻿using MenuRestaurant.Data;
+using MenuRestaurant.Interface;
+using MenuRestaurant.Models;
+
+namespace MenuRestaurant.Repository
+{
+    public class MenuRepository : IMenuRepository
+    {
+        private readonly DataContext _context;
+        public MenuRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Menu DeleteMenuById(Guid Id)
+        {
+            var menu = _context.Menus.Find(Id);
+            _context.Menus.Remove(menu);
+            _context.SaveChanges();
+            return menu;
+        }
+
+        public List<Menu> GetAllMenus()
+        {
+            var menus = _context.Menus.OrderBy(i => i.Id).ToList();
+            return menus;
+        }
+
+        public Menu GetMenuById(Guid Id)
+        {
+            var menu = _context.Menus.Find(Id);
+            return menu;
+        }
+
+        public Menu PostMenu(Menu menu)
+        {
+            _context.Menus.Add(menu);
+            _context.SaveChanges();
+            return menu;
+        }
+
+        public Menu UpdateMenuById(Guid Id, Menu menu)
+        {
+            var menuUpdated = _context.Menus.Find(Id);
+            menuUpdated.FoodName = menu.FoodName;
+            menuUpdated.FoodType = menu.FoodType;
+            menuUpdated.FoodCourse = menu.FoodCourse;
+            _context.SaveChanges();
+            return menuUpdated;
+        }
+    }
+}
